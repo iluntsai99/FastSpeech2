bc39e005ff1ea41333e280e92f0e92bb25e32a70
iluntsai99 <b06902135@ntu.edu.tw>
2021-07-16 01:35:23 +0800

    result files

--- run_train.sh
+++ run_train.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 
-python3 train.py -p config/$1/preprocess.yaml -m config/$1/model.yaml -t config/$1/train.yaml
+python3 train.py -p config/$1/preprocess.yaml -m config/$1/model.yaml -t config/$1/train.yaml --restore_step $2
